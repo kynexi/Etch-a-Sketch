beb946694a4919df42b395f77be11468f9a4330a
kynexi <184446386+kynexi@users.noreply.github.com>
2025-08-11 21:16:07 +0300

    add rainbow toggle

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const container = document.querySelector(".container");
 const newGridBtn = document.querySelector(".gridBtn");
+const rainbowToggle = document.querySelector(".rainbow");
+let rainbow = 0;
+rainbowToggle.addEventListener("click", () => {
+    if (rainbow == 0){ rainbow = 1}
+    else {rainbow  = 0};
+});
+
+function randomColor(){
+    return Math.floor(Math.random() * 256);
+}
+
+function colorGrid(div){
+    if(rainbow == 0 ){
+        div.classList.add('blackBgColor');
+        return;
+    }
+    div.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
+}
 
 function createGrid(x){
     const containerSize = 512; 
@@ -10,7 +28,7 @@ function createGrid(x){
         newDiv.style.flexBasis = `${100 / x}%`;
         newDiv.style.height = `${blockSize}px`;
         newDiv.addEventListener("mouseover", () => {
-            newDiv.classList.add('blackBgColor');
+            colorGrid(newDiv);
         });
         container.appendChild(newDiv);
     }
