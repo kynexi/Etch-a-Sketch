c1c00f31ee5a5350a5c29d2f719e91866187a526
kynexi <184446386+kynexi@users.noreply.github.com>
2025-08-12 16:05:08 +0300

    Add reset button. Add opacity functionality.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const container = document.querySelector(".container");
 const newGridBtn = document.querySelector(".gridBtn");
 const rainbowToggle = document.querySelector(".rainbow");
+const resetBtn = document.querySelector(".reset");
+
 let rainbow = 0;
+let gridSize = 16;
+
 rainbowToggle.addEventListener("click", () => {
     if (rainbow == 0){ rainbow = 1}
     else {rainbow  = 0};
 });
+
+resetBtn.addEventListener("click", () => {
+    resetGrid();
+});
+
 
 function randomColor(){
     return Math.floor(Math.random() * 256);
@@ -16,7 +25,8 @@ function colorGrid(div){
         div.classList.add('blackBgColor');
         return;
     }
-    div.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
+    div.style.backgroundColor = `rgb(${randomColor()}, 
+    ${randomColor()}, ${randomColor()})`;
 }
 
 function createGrid(x){
@@ -28,28 +38,31 @@ function createGrid(x){
         newDiv.style.flexBasis = `${100 / x}%`;
         newDiv.style.height = `${blockSize}px`;
         newDiv.addEventListener("mouseover", () => {
+            let currentOpacity = parseFloat(newDiv.style.opacity) || 0;
+            currentOpacity = Math.min(currentOpacity + 0.1, 1);
+            newDiv.style.opacity = currentOpacity;
             colorGrid(newDiv);
         });
         container.appendChild(newDiv);
     }
 }
 
-function removeGrid(){
+function resetGrid(){
     let child = container.lastElementChild;
     while(child){
         container.removeChild(child);
         child = container.lastElementChild;
     }
+    createGrid(gridSize);
 }
 
 function promptGrid(){
-    let size = prompt("Pick the size of the new grid: ");
-    if(size > 100 || size < 0){
+    gridSize = prompt("Pick the size of the new grid: ");
+    if(gridSize > 100 || gridSize < 0){
         promptGrid();
         return;
     }
-    removeGrid();
-    createGrid(size);
+    resetGrid();
 }
 
-createGrid(16);
+createGrid(gridSize);
